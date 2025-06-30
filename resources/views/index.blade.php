df5703719492b222f11ba33a0dabd7746e29c4fb
Zakia Mayta <2331730117@student.polinema.ac.id>
2025-06-30 11:41:40 +0700

    halaman-zia acc kak

---
+++ resources/views/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Daftar Produk</title>
+</head>
+<body>
+    <h1>Daftar Produk</h1>
+    <ul>
+        @foreach ($produk as $item)
+            <li>{{ $item['nama'] }} - Rp{{ number_format($item['harga'], 0, ',', '.') }}</li>
+        @endforeach
+    </ul>
+</body>
+</html>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Daftar Produk</title>
+</head>
+<body>
+    <h1>Daftar Produk</h1>
+    <ul>
+        @foreach ($produk as $item)
+            <li>{{ $item['nama'] }} - Rp{{ number_format($item['harga'], 0, ',', '.') }}</li>
+        @endforeach
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,12 @@ Route::get('/home', function () {
     return view('home');
 });
 
+Route::get('/produk', function () {
+    $produk = [
+        ['nama' => 'Laptop', 'harga' => 15000000],
+        ['nama' => 'Keyboard', 'harga' => 300000],
+        ['nama' => 'Mouse', 'harga' => 150000],
+    ];
+    
+    return view('produk.index', ['produk' => $produk]);
+});
