779a48d345b85a87d9ba555f94f2b52db4a573f0
Aquaaa <amalia160718@gmail.com>
2025-07-01 10:46:19 +0700

    membuat halaman login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman LOGIN ADMIN</title>
+</head>
+<body>
+    <h1>Selamat Datang di Halaman Index</h1>
+    <p>Ini adalah tampilan Blade Laravel sederhana.</p>
+    <p>BUKU TAMU</p>
+    <p>Welcome Fellas!!!!!!!!!!!!!!!!!!!!!!</p>
+    <p>anu tamu</p>
+</body>
+</html>
