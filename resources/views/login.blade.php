ee0672c8c67d6eb9b409a6e07e4c427ffd575fd4
Aquaaa <amalia160718@gmail.com>
2025-07-01 14:21:18 +0700

    ngedit login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,65 +1,37 @@
-<?php
-session_start();
-require('database.php');
-
-$base_url = "http://localhost/simple_login_php";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-  $email    = $_POST["email"];
-  $password = $_POST["password"];
-
-  $result_login = login($email, $password);
-
-  if (count($result_login) > 0) {
-
-    $_SESSION["data_login"] = $result_login;
-
-    header("Location: /simple_login_php");
-    die();
-  } else {
-    $message = "Email atau password salah <br>";
-  }
-
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>Login Form</title>
-  <style>
-    body {
-      text-align: center;
-    }
-    form {
-      display: inline-block;
-    }
-  </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Login Admin</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<form action="" method="post">
-  <?php !empty($message) ? print $message : ""?>
-
-  <label><h1>Login ke Akun</h1></label>
-
-  <label>Email :</label>
-  <input type="email" name="email">
-  <br><br>
-
-  <label>Password :</label>
-  <input type="password" name="password">
-  <br><br>
-
-  <button type="submit">Login</button>
-  <br>
-  <br>
-
-  <label>
-    <a href="<?= $base_url . '/registration.php'?>">Daftar akun baru</a>
-  </label>
-</form>
+    <div class="container"><br>
+        <div class="col-md-4 col-md-offset-4">
+            <h2 class="text-center"><b>ANISA</b><br>Aplikasi Nilai Santri</h3>
+            <hr>
+            @if(session('error'))
+            <div class="alert alert-danger">
+                <b>Opps!</b> {{session('error')}}
+            </div>
+            @endif
+            <form action="{{ route('actionlogin') }}" method="post">
+            @csrf
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="email" name="email" class="form-control" placeholder="Email" required="">
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password" class="form-control" placeholder="Password" required="">
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">Log In</button>
+                <hr>
+                <p class="text-center">Belum punya akun? <a href="#">Register</a> sekarang!</p>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
