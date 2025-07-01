38c1cc48bc4b7cad3147e40f2a6989f680f44038
Aquaaa <amalia160718@gmail.com>
2025-07-01 08:26:32 +0700

    baru

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman Index</title>
+</head>
+<body>
+    <h1>Selamat Datang di Halaman Index</h1>
+    <p>Ini adalah tampilan Blade Laravel sederhana.</p>
+    <p>BUKU TAMU</p>
+    <p>Welcome Fellas!!!!!!!!!!!!!!!!!!!!!!</p>
+</body>
+</html>
