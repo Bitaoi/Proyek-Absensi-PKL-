71cafaa89ae9dfcbf8361bd2d6c2fda157364b7b
Aquaaa <amalia160718@gmail.com>
2025-07-01 08:48:43 +0700

    yeswart

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,7 @@
 <body>
     <h1>Selamat Datang di Halaman Index</h1>
     <p>Ini adalah tampilan Blade Laravel sederhana.</p>
-
+    <p>BUKU TAMU</p>
+    <p>Welcome Fellas!!!!!!!!!!!!!!!!!!!!!!</p>
 </body>
 </html>
